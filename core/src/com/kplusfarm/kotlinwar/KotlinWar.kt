8c48f1c9b1db7bbe23eb71b92e836d2cb2d7cb8d
kamedon <hkamei06@gmail.com>
2015-07-25 19:04:30 +0900

    mainscreenを作成

--- core/src/com/kplusfarm/kotlinwar/KotlinWar.kt
+++ core/src/com/kplusfarm/kotlinwar/KotlinWar.kt
@@ -6,22 +6,18 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.kplusfarm.kotlinwar.screen.MainScreen
 import kotlin.properties.Delegates
 
 public class KotlinWar : Game() {
-    var batch: SpriteBatch by Delegates.notNull()
-    var img: Texture  by Delegates.notNull()
 
     override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        setScreen(MainScreen(this))
     }
 
     override fun render() {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
+        super.render()
     }
 }
---
+++ core/src/com/kplusfarm/kotlinwar/screen/MainScreen.kt
@@ -0,0 +1,10 @@
+package com.kplusfarm.kotlinwar.screen
+
+import com.badlogic.gdx.ScreenAdapter
+import com.kplusfarm.kotlinwar.KotlinWar
+
+/**
+ * Created by kamedon on 7/25/15.
+ */
+public class MainScreen(game: KotlinWar) : BaseScreen(game) {
+}
