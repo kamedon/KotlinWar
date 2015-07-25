2a566b26e4ea7efe86dc96aa613d24d4af073807
kamedon <hkamei06@gmail.com>
2015-07-25 20:30:59 +0900

    fonttypeを試した

--- core/src/com/kplusfarm/kotlinwar/KotlinWar.kt
+++ core/src/com/kplusfarm/kotlinwar/KotlinWar.kt
@@ -3,18 +3,24 @@ package com.kplusfarm.kotlinwar
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
 import com.kplusfarm.kotlinwar.screen.MainScreen
 import com.kplusfarm.kotlinwar.service.CameraService
 import kotlin.properties.Delegates
 
 public class KotlinWar : Game() {
     var camera: CameraService by Delegates.notNull()
+    var asset: AssetManager by Delegates.notNull()
 
     override fun create() {
+        asset = AssetManager()
         camera = CameraService(540f);
+
+
         setScreen(MainScreen(this))
     }
 
--- core/src/com/kplusfarm/kotlinwar/screen/MainScreen.kt
+++ core/src/com/kplusfarm/kotlinwar/screen/MainScreen.kt
@@ -1,13 +1,48 @@
 package com.kplusfarm.kotlinwar.screen
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.ScreenAdapter
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGeneratorLoader
+import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader
 import com.kplusfarm.kotlinwar.KotlinWar
+import kotlin.properties.Delegates
 
 /**
  * Created by kamedon on 7/25/15.
  */
 public class MainScreen(game: KotlinWar) : BaseScreen(game) {
+    private var batch: SpriteBatch by Delegates.notNull()
+
     override fun show() {
-        super.show()
+
+        val resolver = InternalFileHandleResolver();
+        game.asset.setLoader(javaClass<FreeTypeFontGenerator>(), FreeTypeFontGeneratorLoader(resolver));
+        game.asset.setLoader(javaClass<BitmapFont>(), ".ttf", FreetypeFontLoader(resolver));
+
+        val size1Params = FreetypeFontLoader.FreeTypeFontLoaderParameter();
+        size1Params.fontFileName = "font/font.ttf";
+        size1Params.fontParameters.size = 30;
+        game.asset.load("font/font.ttf", javaClass<BitmapFont>(), size1Params);
+        batch = SpriteBatch();
+
+    }
+
+    override fun render(delta: Float) {
+        game.camera.gameCamera.update();
+        batch.setProjectionMatrix(game.camera.gameCamera.combined);
+        if (game.asset.update() ) {
+            batch.begin();
+            val font = game.asset.get("font/font.ttf", javaClass<BitmapFont>())
+            font.draw(batch, "First font!", game.camera.width / 2, game.camera.height / 2);
+            batch.end();
+        }
+    }
+
+    override fun hide() {
+        game.asset.unload("font/yutapon.ttf")
     }
 }
