9d3d8bf1007ed5474243cec831c9f7eeed65389e
kamedon <hkamei06@gmail.com>
2015-07-25 19:10:09 +0900

    カメラの設定

--- core/src/com/kplusfarm/kotlinwar/KotlinWar.kt
+++ core/src/com/kplusfarm/kotlinwar/KotlinWar.kt
@@ -7,11 +7,14 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.kplusfarm.kotlinwar.screen.MainScreen
+import com.kplusfarm.kotlinwar.service.CameraService
 import kotlin.properties.Delegates
 
 public class KotlinWar : Game() {
+    var camera: CameraService by Delegates.notNull()
 
     override fun create() {
+        camera = CameraService(540f);
         setScreen(MainScreen(this))
     }
 
@@ -19,5 +22,11 @@ public class KotlinWar : Game() {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         super.render()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        super.resize(width, height)
+        camera.gameViewport.update(width, height)
+        camera.uiViewport.update(width, height)
     }
 }
--- core/src/com/kplusfarm/kotlinwar/screen/MainScreen.kt
+++ core/src/com/kplusfarm/kotlinwar/screen/MainScreen.kt
@@ -7,4 +7,7 @@ import com.kplusfarm.kotlinwar.KotlinWar
  * Created by kamedon on 7/25/15.
  */
 public class MainScreen(game: KotlinWar) : BaseScreen(game) {
+    override fun show() {
+        super.show()
+    }
 }
